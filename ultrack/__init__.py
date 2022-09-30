ec583ae5506a811eda9cd9651e399ea90bea3161
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2022-09-30 14:50:14 -0700

    add stardist 2d demo

--- ultrack/__init__.py
+++ ultrack/__init__.py
@@ -1,5 +1,6 @@
 from ultrack.core.export.ctc import to_ctc
 from ultrack.core.export.tracks_layer import to_tracks_layer
+from ultrack.core.export.zarr import tracks_to_zarr
 from ultrack.core.linking.processing import link
 from ultrack.core.segmentation.processing import segment
 from ultrack.core.tracking.processing import track
