747919210803af78626611c348c50cafc38d10f9
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2023-08-01 13:35:46 -0700

    add get_subgraph function

--- ultrack/analysis/_test/test_analysis_utils.py
+++ ultrack/analysis/_test/test_analysis_utils.py
@@ -2,9 +2,11 @@ from typing import Dict, List
 
 import numpy as np
 import pandas as pd
+import pytest
 from numba import typed, types
 
-from ultrack.analysis.utils import _left_first_search, sort_track_ids
+from ultrack.analysis import get_subgraph, sort_track_ids
+from ultrack.analysis.utils import _left_first_search
 from ultrack.core.database import NO_PARENT
 
 
@@ -47,3 +49,41 @@ def test_sort_track_ids_forest():
     tracks_df = pd.DataFrame(data)
     result = sort_track_ids(tracks_df)
     assert np.array_equal(result, np.array([2, 1, 3, 5, 4, 6, 7]))
+
+
+@pytest.fixture
+# Sample data for testing
+def tracks_df() -> pd.DataFrame:
+    data = {
+        "track_id": [1, 2, 3, 4, 5, 6, 7, 8],
+        "parent_track_id": [NO_PARENT, 1, 1, 2, 2, NO_PARENT, 6, 6],
+    }
+    return pd.DataFrame(data)
+
+
+def test_get_subgraph_single_track_id(tracks_df: pd.DataFrame):
+    track_ids = [3]
+    lineage_ids = [1, 2, 3, 4, 5]
+    result_df = get_subgraph(tracks_df, track_ids)
+    expected_df = tracks_df[tracks_df["track_id"].isin(lineage_ids)]
+    pd.testing.assert_frame_equal(result_df, expected_df)
+
+
+def test_get_subgraph_multiple_track_ids(tracks_df: pd.DataFrame):
+    track_ids = [2, 3, 5]
+    lineage_ids = [1, 2, 3, 4, 5]
+    result_df = get_subgraph(tracks_df, track_ids)
+    expected_df = tracks_df[tracks_df["track_id"].isin(lineage_ids)]
+    pd.testing.assert_frame_equal(result_df, expected_df)
+
+
+def test_get_subgraph_from_different_trees(tracks_df: pd.DataFrame):
+    track_ids = [3, 7]
+    result_df = get_subgraph(tracks_df, track_ids)
+    pd.testing.assert_frame_equal(result_df, tracks_df)
+
+
+def test_get_subgraph_empty_subgraph(tracks_df: pd.DataFrame):
+    track_ids = [9, 10, 11]
+    result_df = get_subgraph(tracks_df, track_ids)
+    assert result_df.empty
