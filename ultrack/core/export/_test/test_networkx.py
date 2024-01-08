571c0fa9465a1e917efaccef1ec78c600d85109f
Jordão Bragantini <jordao.bragantini@czbiohub.org>
2024-01-08 15:54:53 -0800

    added networkx export functionality (#23)

---
+++ ultrack/core/export/_test/test_networkx.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import pytest
+
+from ultrack.core.database import NO_PARENT
+from ultrack.core.export import tracks_layer_to_networkx
+
+
+@pytest.mark.parametrize("children_to_parent", [True, False])
+def test_to_networkx(
+    children_to_parent: bool,
+) -> None:
+    tracks_df = pd.DataFrame(
+        {
+            "id": [1, 2, 3, 4],
+            "parent_id": [NO_PARENT, 1, 2, 2],
+            "track_id": [1, 1, 2, 3],
+            "t": [0, 1, 2, 2],
+            "z": [0, 0, 0, 0],
+            "y": [10, 20, 30, 10],
+            "x": [1, 2, 3, 1],
+        }
+    )
+    tracks_df.set_index("id", inplace=True)
+
+    graph = tracks_layer_to_networkx(tracks_df, children_to_parent=children_to_parent)
+
+    assert len(graph.nodes) == tracks_df.shape[0]
+    assert len(graph.edges) == tracks_df.shape[0] - 1
+
+    for node, data in graph.nodes(data=True):
+        assert node in tracks_df.index
+        for key, value in data.items():
+            assert value == tracks_df.loc[node, key]
+
+    if children_to_parent:
+        for edge in [(2, 1), (3, 2), (4, 2)]:
+            assert edge in graph.edges
+    else:
+        for edge in [(1, 2), (2, 3), (2, 4)]:
+            assert edge in graph.edges
+
+
+def test_to_networkx_without_parent_id() -> None:
+    tracks_df = pd.DataFrame(
+        {
+            "id": [1, 2, 3, 4],
+            "track_id": [1, 1, 2, 3],
+            "t": [0, 1, 2, 2],
+            "z": [0, 0, 0, 0],
+            "y": [10, 20, 30, 10],
+            "x": [1, 2, 3, 1],
+        }
+    )
+    tracks_df.set_index("id", inplace=True)
+
+    graph = tracks_layer_to_networkx(tracks_df)
+
+    assert len(graph.nodes) == tracks_df.shape[0]
+    assert len(graph.edges) == 1
+
+    for node in graph.nodes:
+        assert node in tracks_df.index
+
+    assert (1, 2) in graph.edges
---
+++ ultrack/core/export/networkx.py
@@ -0,0 +1,88 @@
+import logging
+
+import networkx as nx
+import pandas as pd
+
+from ultrack.config.config import MainConfig
+from ultrack.core.database import NO_PARENT
+from ultrack.core.export.tracks_layer import to_tracks_layer
+from ultrack.tracks.graph import _create_tracks_forest
+
+LOG = logging.getLogger(__name__)
+
+
+def tracks_layer_to_networkx(
+    tracks_df: pd.DataFrame,
+    children_to_parent: bool = False,
+) -> nx.DiGraph:
+    """
+    Convert napari tracks layer tracks dataframe to networkx directed graph.
+    By default, the edges are the parent to child relationships.
+
+    Parameters
+    ----------
+    config : MainConfig
+        Configuration parameters.
+    children_to_parent : bool
+        If set, edges encode child to parent relationships.
+
+    Returns
+    -------
+    nx.DiGraph
+        Networkx graph.
+    """
+    graph = nx.DiGraph()
+
+    for index, row in tracks_df.iterrows():
+        graph.add_node(index, **row.to_dict())
+
+    if "parent_id" not in tracks_df.columns:
+        LOG.warning(
+            "Parent id not found in tracks dataframe. Divisions won't be exported."
+        )
+        for _, group in tracks_df.groupby("track_id"):
+            for i in range(len(group) - 1):
+                if children_to_parent:
+                    graph.add_edge(group.index[i + 1], group.index[i])
+                else:
+                    graph.add_edge(group.index[i], group.index[i + 1])
+
+    else:
+        dict_graph = _create_tracks_forest(
+            tracks_df.index.values,
+            tracks_df["parent_id"].to_numpy(int),
+        )
+        dict_graph.pop(NO_PARENT)
+
+        for parent, children in dict_graph.items():
+            for child in children:
+                if children_to_parent:
+                    graph.add_edge(child, parent)
+                else:
+                    graph.add_edge(parent, child)
+
+    return graph
+
+
+def to_networkx(
+    config: MainConfig,
+    children_to_parent: bool = False,
+) -> nx.DiGraph:
+    """
+    Convert solution from database to networkx directed graph.
+    By default, the edges are the parent to child relationships.
+
+    Parameters
+    ----------
+    config : MainConfig
+        Configuration parameters.
+    children_to_parent : bool
+        If set, edges encode child to parent relationships.
+
+    Returns
+    -------
+    nx.DiGraph
+        Networkx graph.
+    """
+    df = to_tracks_layer(config)
+    return tracks_layer_to_networkx(df, children_to_parent)
