ba15343630cb0a08409ebab80d15364ab35a7704
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-01-26 13:33:20 -0800

    fixed gurobi check and added gurobi dependency

--- ultrack/cli/check_gurobi.py
+++ ultrack/cli/check_gurobi.py
@@ -14,13 +14,13 @@ def check_gurobi_cli() -> None:
         gp.Env()
         print("Gurobi environment created successfully.")
 
-    except gp.GurobiError as e:
-        print(f"GurobiError encountered: {e.message}")
-        print("There may be a problem with your Gurobi license or setup.")
     except ImportError:
         print(
             "Gurobi module not found. Ensure it is installed and the Python path is set correctly."
         )
         print("Using `conda install gurobi -c gurobi` is recommended.")
+    except gp.GurobiError as e:
+        print(f"GurobiError encountered: {e.message}")
+        print("There may be a problem with your Gurobi license or setup.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
