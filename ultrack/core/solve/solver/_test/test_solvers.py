c235d37f205ba83542c8824d8cbba145eed999ec
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2025-01-24 07:26:34 -0800

    Handling cell exiting in/out the border of the image frame (#94)
    * initial commit
    
    * adding functionality to not penalize the (zyx) tracks
    
    * - ensuring border_distance and shape work for TYX and TZYX
    - eliminating itterrows logic
    - fixing logging format
    - setting image_border_size default None
    
    * - abstracted function to check borders
    - added a test for the is_border case
    
    * pending bug as `_check_inside_border` was moved out of SQLTracking
    
    * removing redundant list and returning np.array instead of pd.series()
    
    * minor style changes
    
    * fixing testing
    
    * fix tox versions
    
    * reduced inside border func
    
    * fixing bug indexing
    
    ---------
    
    Co-authored-by: Jordao Bragantini <jordao.bragantini@czbiohub.org>

--- ultrack/core/solve/solver/_test/test_solvers.py
+++ ultrack/core/solve/solver/_test/test_solvers.py
@@ -4,6 +4,7 @@ import pytest
 
 from ultrack.config.config import MainConfig
 from ultrack.core.solve.solver.mip_solver import MIPSolver
+from ultrack.core.solve.sqltracking import _check_inside_border
 
 
 @pytest.mark.parametrize(
@@ -283,3 +284,73 @@ def test_fixed_edges_constraint_solver(config_instance: MainConfig) -> None:
         expected_solution.loc[solution.index, "parent_id"] == solution["parent_id"]
     )
     assert np.allclose(objective, weights[expected_edges].sum())
+
+
+@pytest.mark.parametrize(
+    "config_content",
+    [
+        {
+            "tracking.image_border_size": (1, 3, 2),
+        },
+    ],
+    indirect=True,
+)
+def test_add_nodes_with_is_border(config_instance: MainConfig):
+    """
+    Test the add_nodes method with the is_border parameter.
+
+    Assuming TZYX shape = (10,10,10,10) and varying image_border_size
+    Testing if node is inside the border in all combinations of z, y, x
+    """
+    solver = MIPSolver(config_instance.tracking_config)
+
+    shape = (10, 10, 10, 10)  # TZYX
+
+    data = {
+        "id": np.arange(10),
+        "t": np.concatenate([np.zeros(5), np.ones(5)]),
+        "z": np.array([0, 0, 0, 0, 5, 0, 5, 9, 5, 9]),
+        "y": np.array([5, 2, 2, 0, 5, 5, 2, 9, 5, 5]),
+        "x": np.array([5, 5, 5, 0, 1, 1, 1, 9, 5, 9]),
+    }
+    df = pd.DataFrame(data)
+    is_border = _check_inside_border(
+        df, config_instance.tracking_config.image_border_size, shape=shape
+    )
+
+    # Assert that the is_border Series is as expected
+    expected_is_border = np.asarray(
+        [True, True, True, True, True, True, True, True, False, True]
+    )
+    np.testing.assert_array_equal(is_border, expected_is_border)
+
+    # Add nodes to the solver
+    solver.add_nodes(
+        df["id"],
+        # avoiding first and last nodes zero (dist)appearance cost
+        np.zeros(10, dtype=bool),
+        np.zeros(10, dtype=bool),
+        is_border=is_border,
+    )
+
+    # Assertions to verify the nodes were added correctly
+    assert solver._nodes is not None
+    assert solver._appearances is not None
+    assert solver._disappearances is not None
+    assert solver._divisions is not None
+
+    # Verify that the is_border values are correctly processed
+    for idx, border in enumerate(is_border):
+        if border:
+            # Verify that appearance and disappearance weights are set to 0 for border nodes
+            assert solver._appearances[idx].obj == 0
+            assert solver._disappearances[idx].obj == 0
+        else:
+            assert (
+                solver._appearances[idx].obj
+                == config_instance.tracking_config.appear_weight
+            )
+            assert (
+                solver._disappearances[idx].obj
+                == config_instance.tracking_config.disappear_weight
+            )
