fd0dfe9c64db1fdb5931b957e969bfffe25619bc
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-01-30 09:09:25 -0800

    replacing networkx export from 'records' to 'index'

--- ultrack/core/export/networkx.py
+++ ultrack/core/export/networkx.py
@@ -40,8 +40,7 @@ def tracks_layer_to_networkx(
     else:
         tracks_df = tracks_df.set_index("id")
 
-    records = tracks_df.to_dict(orient="records")
-    for index, row in enumerate(records):
+    for index, row in tracks_df.to_dict(orient="index").items():
         graph.add_node(index, **row)
 
     if "parent_id" not in tracks_df.columns:
