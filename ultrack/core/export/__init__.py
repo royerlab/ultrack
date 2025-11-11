5824ece84d288401fbd832072bd5da761ff03118
TeunHuijben <teun.huijben@czbiohub.org>
2025-11-10 16:45:50 -0800

    updated to_geff export function and added cli

--- ultrack/core/export/__init__.py
+++ ultrack/core/export/__init__.py
@@ -1,6 +1,6 @@
 from ultrack.core.export.ctc import to_ctc
 from ultrack.core.export.exporter import export_tracks_by_extension
-from ultrack.core.export.geff import to_geff
+from ultrack.core.export.geff import to_geff, to_geff_from_database
 from ultrack.core.export.networkx import to_networkx, tracks_layer_to_networkx
 from ultrack.core.export.trackmate import to_trackmate, tracks_layer_to_trackmate
 from ultrack.core.export.tracks_layer import to_tracks_layer
